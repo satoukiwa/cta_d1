88e94895a9f0003efc45edd18e407961b3dbe765
Raghu Betina <raghu@firstdraft.com>
2018-06-05 11:38:23 +0000

    Whitelist columns in TrainStatus for ActiveAdmin

--- app/admin/train_statuses.rb
+++ app/admin/train_statuses.rb
@@ -1,4 +1,6 @@
 ActiveAdmin.register TrainStatus do
+
+ permit_params :train_number, :status
 # See permitted parameters documentation:
 # https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
 #
